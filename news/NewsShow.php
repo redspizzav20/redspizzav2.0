5652503da69b022434c82475f6d39c8af299eb1e
nhan <thiennhant95@gmail.com>
2016-12-16 12:36:29 +0700

    da sua file newsshow

--- news/NewsShow.php
+++ news/NewsShow.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -16,6 +16,7 @@
 	}
 		$sql="SELECT * FROM news";
 		$stm = $db->Selectquery($sql);
+// da sua loi	
 ?>
 <body>
 	<div class="content-wrapper">
